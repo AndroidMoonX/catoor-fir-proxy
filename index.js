ff37612acb0f8f3d440d2e49cda18e261102ef85
Danny Hdz <161549383+AndroidMoonX@users.noreply.github.com>
2025-08-12 20:02:27 -0600

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ mqttClient.on('connect', () => {
   mqttClient.subscribe('catoor/servoState/set');
 
   // Solo iniciar servidor cuando MQTT esté listo
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 8080;
   app.listen(PORT, () => {
     console.log(`Servidor escuchando en puerto ${PORT}`);
   });
